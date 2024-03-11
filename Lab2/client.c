26fed4615105761de55078d48a11f802b8059953
Wiktor <wiktord2003@wp.pl>
2024-03-11 23:41:57 +0100

    Complete final Lab2

--- Lab2/client.c
+++ Lab2/client.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
-#include "collatz.c"
+
+#ifndef DYNAMIC_DLOPEN
+    #include "collatz.c"
+#endif
+
+#ifdef DYNAMIC_DLOPEN
+    #include "dlfcn.h"
+    int (*collatz_conjecture)(int input);
+    int (*test_collatz_convergence)(int input, int max_iter);
+#endif
 
 int main() {
+    #ifdef DYNAMIC_DLOPEN
+    void *dlhandle = dlopen("./libcollatz.so", RTLD_LAZY);
+    if (!dlhandle) {
+        printf("Library Opening Error!\n");
+        return 0;
+    }
+
+    collatz_conjecture = dlsym(dlhandle, "collatz_conjecture");
+    test_collatz_convergence = dlsym(dlhandle, "test_collatz_convergence");
+    #endif
+
+    printf("%d\n", collatz_conjecture(10));
+
     int result1 = test_collatz_convergence(10, 100);
     int result2 = test_collatz_convergence(11, 100);
     int result3 = test_collatz_convergence(12, 100);
@@ -15,4 +37,5 @@ int main() {
     if (result3 == 10) {
         printf("%d\n", result3);
     }
+    return 0;
 }
