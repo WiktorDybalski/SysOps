08e6b3351232767197d3b0331b8dc240e9f4869e
Wiktor <wiktord2003@wp.pl>
2024-03-18 23:48:02 +0100

    Add completed Lab3

--- Lab3/find.c
+++ Lab3/find.c
@@ -1,19 +1,30 @@
-#include <stdlib.h>
-#include <dirent.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
 
 int main() {
-
-    DIR *curr_dir = opendir("dir_to_check");
+    DIR *curr_dir = opendir(".");
     if (curr_dir == NULL) {
-        perror("Nie mozna otworzyc folderu");
+        perror("Opening Folder Error");
         return -1;
     }
 
+    long long total_size = 0;
     struct dirent *entry;
+    struct stat fileStat;
+
     while ((entry = readdir(curr_dir)) != NULL) {
-        printf("%s\n", entry->d_name);
+        if (stat(entry->d_name, &fileStat) ==  -1) {
+                perror("stat error");
+                continue;
+        }
+        if(!S_ISDIR(fileStat.st_mode)) {
+            printf("File: %s, Size: %lld bytes\n", entry->d_name, (long long)fileStat.st_size);
+            total_size += fileStat.st_size;
+        }
     }
     closedir(curr_dir);
+    printf("\nTotal size: %lld\n", total_size);
     return 0;
 }
