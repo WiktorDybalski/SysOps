ccd9369be67bc1e8d988032da51c358ff8b45d80
Wiktor <wiktord2003@wp.pl>
2024-04-09 22:24:50 +0200

    Completed Lab6

--- Lab6/task1.c
+++ Lab6/task1.c
@@ -1,13 +1,13 @@
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <wait.h>
-#include <sys/time.h>
+#include <time.h>
 
 double func(double x) {
     return 4.0 / (x * x + 1);
 }
+
 double calculate_small_interval(double start, double end, double width) {
     double sum = 0.0;
     for (double x = start; x < end; x += width) {
@@ -24,11 +24,15 @@ int main(int argc, char *argv[]) {
 
     double width = atof(argv[1]);
     int number_of_proc = atoi(argv[2]);
+    int total_intervals = (int)(1.0 / width);
+    int intervals_per_proc = total_intervals / number_of_proc;
+    int remaining_intervals = total_intervals % number_of_proc;
+
     double total = 0.0;
     int pipes[number_of_proc][2];
 
-    struct timeval time_start, time_end;
-    gettimeofday(&time_start, NULL);
+    clock_t time_start, time_end;
+    time_start = clock();
 
     for (int i = 0; i < number_of_proc; i++) {
         if (pipe(pipes[i]) == -1) {
@@ -38,9 +42,11 @@ int main(int argc, char *argv[]) {
         pid_t pid = fork();
         if (pid == 0) {
             close(pipes[i][0]);
-
-            double start = i * (1.0 / number_of_proc);
-            double end = (i + 1) * (1.0 / number_of_proc);
+            double start = i * intervals_per_proc * width;
+            double end = (i + 1) * intervals_per_proc * width;
+            if (i == number_of_proc - 1) {
+                end += remaining_intervals * width;
+            }
             double result = calculate_small_interval(start, end, width);
             write(pipes[i][1], &result, sizeof(result));
             close(pipes[i][1]);
@@ -56,21 +62,24 @@ int main(int argc, char *argv[]) {
         total += result;
         close(pipes[i][0]);
     }
-    gettimeofday(&time_end, NULL);
-    double execution_time = (time_end.tv_sec - time_start.tv_sec) + (time_end.tv_usec - time_start.tv_usec) / 1000000.0;
+
     for (int i = 0; i < number_of_proc; i++) {
         wait(NULL);
     }
 
+    time_end = clock();
+    double execution_time = ((double)(time_end - time_start)) / CLOCKS_PER_SEC;
+
     FILE *time_file = fopen("time_file.txt", "a");
     if (time_file == NULL) {
         printf("There is no such file");
+    } else {
+        fprintf(time_file, "Time: %lf, Amount of processes: %d, Width: %lf\n", execution_time, number_of_proc, width);
         fclose(time_file);
     }
-    fprintf(time_file, "Time: %lf, Amount of processes: %d, Width: %lf\n", execution_time, number_of_proc, width);
-    fclose(time_file);
 
     printf("Width of sub-intervals: %lf, Number of processes: %d\n", width, number_of_proc);
     printf("Execution time: %.6f seconds\n", execution_time);
     printf("Integration result: %lf\n", total);
+    return 0;
 }
