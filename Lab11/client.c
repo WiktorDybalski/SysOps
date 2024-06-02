4c352a3ef2d87f79354e4c3f22fe9e66172b8702
Wiktor <wiktord2003@wp.pl>
2024-06-02 17:53:02 +0200

    Update Lab11

--- Lab11/client.c
+++ Lab11/client.c
@@ -4,17 +4,27 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <signal.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 1024
 
 int client_socket;
 int client_id;
+int child_pid;
 char client_name[BUFFER_SIZE];
 
 void handle_sigint(int sig) {
     char buffer[BUFFER_SIZE];
-    snprintf(buffer, sizeof(buffer), "STOP %d", client_id);
+    snprintf(buffer, sizeof(buffer), "%d:STOP:", client_id);
+    printf("Sending message: %s\n", buffer);
     send(client_socket, buffer, strlen(buffer), 0);
+
+    if (child_pid > 0) {
+        kill(child_pid, SIGTERM);
+        waitpid(child_pid, NULL, 0);
+        printf("KILLING CHILD PROCESS\n");
+    }
+
     close(client_socket);
     exit(0);
 }
@@ -32,6 +42,7 @@ void receive_messages() {
         }
     }
 }
+
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -62,9 +73,6 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    snprintf(buffer, sizeof(buffer), "INIT %s", client_name);
-    send(client_socket, buffer, strlen(buffer), 0);
-
     int bytes_read = recv(client_socket, buffer, BUFFER_SIZE - 1, 0);
     if (bytes_read <= 0) {
         perror("recv");
@@ -84,7 +92,8 @@ int main(int argc, char* argv[]) {
 
     signal(SIGINT, handle_sigint);
 
-    if (fork() == 0) {
+    child_pid = fork();
+    if (child_pid == 0) {
         receive_messages();
         exit(0);
     }
@@ -94,8 +103,10 @@ int main(int argc, char* argv[]) {
         if (len > 0 && buffer[len - 1] == '\n') {
             buffer[len - 1] = '\0';
         }
-        snprintf(buffer, BUFFER_SIZE, "%d %s", client_id, buffer);
-        send(client_socket, buffer, strlen(buffer), 0);
+        char message[BUFFER_SIZE];
+        snprintf(message, BUFFER_SIZE, "%d:%s", client_id, buffer);
+        printf("Sending message: %s\n", message);
+        send(client_socket, message, strlen(message), 0);
     }
 
     close(client_socket);
--- Lab11/server.c
+++ Lab11/server.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
@@ -8,10 +6,12 @@
 #include <time.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/epoll.h>
 
 #define MAX_CLIENTS_NUMBER 4
 #define PING_INTERVAL 10
 #define BUFFER_SIZE 1024
+#define MAX_EVENTS 10
 
 typedef struct {
     int client_id;
@@ -22,6 +22,16 @@ typedef struct {
 
 client_info clients[MAX_CLIENTS_NUMBER];
 int client_count = 0;
+int epoll_fd;
+
+void init_client_info() {
+    for (int i = 0; i < MAX_CLIENTS_NUMBER; i++) {
+        clients[i].client_id = -1;
+        clients[i].sockfd = -1;
+        clients[i].last_ping = 0;
+        clients[i].ping_waiting = 0;
+    }
+}
 
 void handle_init(int client_socket) {
     char msg[BUFFER_SIZE];
@@ -31,20 +41,34 @@ void handle_init(int client_socket) {
         send(client_socket, msg, strlen(msg), 0);
         close(client_socket);
     } else {
-        clients[client_count].sockfd = client_socket;
-        clients[client_count].client_id = client_count;
-        sprintf(msg, "%d", client_count);
-        printf("Creating client with client_id: %d\n", client_count);
-        send(client_socket, msg, strlen(msg), 0);
-        client_count++;
+        for (int i = 0; i < MAX_CLIENTS_NUMBER; i++) {
+            if (clients[i].client_id < 0) {
+                clients[i].sockfd = client_socket;
+                clients[i].client_id = i;
+                clients[i].last_ping = time(NULL);
+                clients[i].ping_waiting = 0;
+
+                struct epoll_event event;
+                event.events = EPOLLIN;
+                event.data.fd = client_socket;
+                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &event);
+
+                sprintf(msg, "%d", i);
+                printf("Creating client with client_id: %d, and sock_fd: %d\n", clients[i].client_id, clients[i].sockfd);
+                send(client_socket, msg, strlen(msg), 0);
+                client_count++;
+                break;
+            }
+        }
     }
 }
 
 void list(int client_socket) {
+    printf("Processing: LIST\n");
     char buffer[BUFFER_SIZE];
-    int offset = snprintf(buffer, sizeof (buffer), "List of clients: \n");
-    for (int i = 0; i < client_count; i++) {
-        if (clients[i].sockfd != 0) {
+    int offset = snprintf(buffer, sizeof(buffer), "List of clients: \n");
+    for (int i = 0; i < MAX_CLIENTS_NUMBER; i++) {
+        if (clients[i].client_id >= 0) {
             int bytes_written = snprintf(buffer + offset, sizeof(buffer) - offset, "Client %d\n", clients[i].client_id);
             offset += bytes_written;
             if (offset >= sizeof(buffer)) {
@@ -52,37 +76,42 @@ void list(int client_socket) {
             }
         }
     }
+    printf("Sending list of clients:\n%s", buffer);
     send(client_socket, buffer, offset, 0);
 }
 
 void to_all(int client_id, char* msg) {
+    printf("Processing: 2ALL\n");
     char buffer[BUFFER_SIZE];
     snprintf(buffer, sizeof(buffer), "Client with ID %d: %s", client_id, msg);
-    for (int i = 0; i < client_count; i++) {
-        if (clients[i].sockfd != 0 && clients[i].client_id != client_id) {
+    for (int i = 0; i < MAX_CLIENTS_NUMBER; i++) {
+        if (clients[i].client_id >= 0 && clients[i].client_id != client_id) {
             send(clients[i].sockfd, buffer, strlen(buffer), 0);
         }
     }
 }
 
 void to_one(int sender_id, int receiver_id, char* msg) {
+    printf("Processing: 2ONE\n");
     char buffer[BUFFER_SIZE];
     snprintf(buffer, sizeof(buffer), "Client with ID %d: %s", sender_id, msg);
-    for (int i = 0; i < client_count; i++) {
-        if (clients[i].sockfd != 0 && clients[i].client_id == receiver_id) {
+    for (int i = 0; i < MAX_CLIENTS_NUMBER; i++) {
+        if (clients[i].client_id == receiver_id) {
             send(clients[i].sockfd, buffer, strlen(buffer), 0);
             break;
         }
     }
 }
 
-void handle_stop(int client_id) {
-    for (int i = 0; i < client_count; i++) {
-        if (clients[i].sockfd != 0 && clients[i].client_id == client_id) {
+void handle_stop(int client_socket) {
+    printf("Processing: STOP\n");
+    for (int i = 0; i < MAX_CLIENTS_NUMBER; i++) {
+        if (clients[i].sockfd == client_socket) {
+            printf("Client with ID %d disconnected\n", clients[i].client_id);
             close(clients[i].sockfd);
-            clients[i].sockfd = 0;
+            clients[i].sockfd = -1;
+            clients[i].client_id = -1;
             client_count--;
-            printf("Client with ID %d disconnected", client_id);
             break;
         }
     }
@@ -116,16 +145,18 @@ void handle_client_message(int client_socket) {
         handle_stop(client_socket);
     } else {
         buffer[bytes_read] = '\0';
-        char* token = strtok(buffer, " ");
+        int client_id = atoi(strtok(buffer, ":"));
+        char* token = strtok(NULL, ":");
+        printf("Received from ClientID %d, message: %s\n", client_id, token);
         if (strcmp(token, "LIST") == 0) {
             list(client_socket);
         } else if (strcmp(token, "2ALL") == 0) {
-            token = strtok(NULL, "");
-            to_all(client_socket, token);
+            token = strtok(NULL, ":");
+            to_all(client_id, token);
         } else if (strcmp(token, "2ONE") == 0) {
-            int receiver_id = atoi(strtok(NULL, " "));
-            token = strtok(NULL, "");
-            to_one(client_socket, receiver_id, token);
+            int receiver_id = atoi(strtok(NULL, ":"));
+            token = strtok(NULL, ":");
+            to_one(client_id, receiver_id, token);
         } else if (strcmp(token, "STOP") == 0) {
             handle_stop(client_socket);
         } else {
@@ -136,7 +167,7 @@ void handle_client_message(int client_socket) {
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <catcher PID> <mode>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <server_address> <server_port>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
     char* address = argv[1];
@@ -171,13 +202,56 @@ int main(int argc, char* argv[]) {
     printf("=================================== Server has started ==========================================\n");
     printf("Address: %s, Port: %d\n", address, port);
 
-    while (1) {
-        int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
-        if (client_socket < 0) {
-            perror("accept");
-            continue;
-        }
+    epoll_fd = epoll_create1(0);
+    if (epoll_fd < 0) {
+        perror("epoll_create1");
+        close(server_socket);
+        exit(EXIT_FAILURE);
     }
+
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLOUT;
+    event.data.fd = server_socket;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_socket, &event) < 0) {
+        perror("epoll_ctl");
+        close(server_socket);
+        close(epoll_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    struct epoll_event events[MAX_EVENTS];
+    init_client_info(clients);
+
+    while (1) {
+        int event_count = epoll_wait(epoll_fd, events, MAX_EVENTS, PING_INTERVAL * 1000);
+        if (event_count < 0) {
+            perror("epoll_wait");
+            close(server_socket);
+            close(epoll_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < event_count; i++) {
+            if (events[i].data.fd == server_socket) {
+                if (events[i].events & EPOLLIN) {
+                    int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &client_addr_len);
+                    if (client_socket < 0) {
+                        perror("accept");
+                        continue;
+                    }
+                    handle_init(client_socket);
+                }
+            } else {
+                if (events[i].events & EPOLLIN) {
+                    handle_client_message(events[i].data.fd);
+                }
+            }
+        }
+
+        //alive();
+    }
+
     close(server_socket);
+    close(epoll_fd);
     return 0;
 }
